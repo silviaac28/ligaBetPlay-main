14a8137dd8cef99df034d7c85bba2a0913bdc5d9
silviaac28 <silviaangarita28@gmail.com>
2024-07-01 22:54:07 -0500

    lesion

--- src/main/java/com/finalbetplay/clases/Lesion.java
+++ src/main/java/com/finalbetplay/clases/Lesion.java
@@ -4,9 +4,70 @@ import java.util.Date;
 
 public class Lesion {
     private int id;
+    private int idJugador;
     private String tipo;
     private String gravedad;
     private Date fechaInicio;
     private Date fechaFin;
+    
+    public Lesion() {
+    }
+
+    public Lesion(int id, int idJugador, String tipo, String gravedad, Date fechaInicio, Date fechaFin) {
+        this.id = id;
+        this.idJugador = idJugador;
+        this.tipo = tipo;
+        this.gravedad = gravedad;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdJugador() {
+        return idJugador;
+    }
+
+    public void setIdJugador(int idJugador) {
+        this.idJugador = idJugador;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getGravedad() {
+        return gravedad;
+    }
+
+    public void setGravedad(String gravedad) {
+        this.gravedad = gravedad;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
 
 }
